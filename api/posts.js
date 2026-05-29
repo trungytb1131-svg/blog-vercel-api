5ccb9315d41390657bc34cfb7e854c928211a963
trungytb1131-svg <trungytb1131@gmail.com>
2026-05-29 09:50:34 +0700

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -38,7 +38,7 @@ module.exports = async (req, res) => {
   }
 
   if (req.method === 'GET') {
-    // Đã thêm content và image vào đây
+    // ĐÃ SỬA: Đã thêm content và image vào đây
     const { rows } = await sql`
       SELECT id, title, content, image, source_url, tags, created_at
       FROM posts ORDER BY created_at DESC LIMIT 50;
